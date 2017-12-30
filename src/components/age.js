d6b6fc889dee122a62d35fc8a8ff67590b99b22d
Dave Brudner <dbrudne1@gmail.com>
2017-12-30 13:36:13 -0500

    commit

---
+++ src/components/age.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Age = () => {
+	<label>
+		Age:
+		<input type="text" name="age" />
+	</label>
+}
---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Age from './age'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+      	<Age />
+      </div>
+    );
+  }
+}
